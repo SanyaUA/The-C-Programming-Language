81ffcb15cf466192ad1feec1b359c49e9ed39fcb
SanyaUA <kozmey1996@mail.ua>
2018-06-18 17:41:41 +0300

    Print table temperature Fahrenheit and Celsius with floats

---
+++ The C Programming Language/Chapter-1/table-2.c
@@ -0,0 +1,27 @@
+/*
+**
+** The output table temperature Fahrenheit and Celsius with float
+** for fahr = 0, 20, ..., 300.
+**
+*/
+#include <stdio.h>
+
+int main()
+{
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step  = 20;
+
+  fahr = lower;
+  while (fahr <= upper)
+  {
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+
+  return 0;
+}
