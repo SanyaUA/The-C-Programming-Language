95d0246cc6920350c776e1bf69f9c8949ec0d0ed
SanyaUA <kozmey1996@mail.ua>
2018-06-19 20:53:10 +0300

    Count the characters in the input stream

---
+++ The C Programming Language/Chapter-1/counter.c
@@ -0,0 +1,19 @@
+/*
+**
+** Count the characters in the input stream.
+**
+*/
+#include <stdio.h>
+
+int main()
+{
+  long nc;
+  
+  nc = 0;
+  while (getchar() != EOF)
+    ++nc;
+
+  printf("%ld\n", nc);
+  
+  return 0;
+}
