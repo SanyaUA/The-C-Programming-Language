1f7cccac84facbbb2e0fb5bb24ff328acb22ed5a
SanyaUA <kozmey1996@mail.ua>
2018-06-28 16:54:57 +0300

    Rewrite loop from getline() function without using && or ||

--- The C Programming Language/Chapter-2/Tasks/task-2.2.c
+++ The C Programming Language/Chapter-2/Tasks/task-2.2.c
@@ -5,10 +5,31 @@
 */
 #include <stdio.h>
 
-void getline(char line[], int lim)
+#define MAXLENGTH 1000
+
+int getlines(char line[], int lim);
+
+int main(void)
 {
-  //...
+  int len;
+  char line[MAXLENGTH];
+
+  while ((len = getlines(line, MAXLENGTH)) > 0)
+  {
+    printf("%s", line);
+  }
+  return 0;
+}
+int getlines(char s[], int lim)
+{
+  int c, i;
   for (i = 0; i < lim - 1, (c = getchar()) != '\n', c != EOF; ++i)
     s[i] = c;
-  //...
+  if (c == '\n')
+  {
+    s[i] = c;
+    ++i;
+  }
+  s[i] = c;
+  return i;
 }
