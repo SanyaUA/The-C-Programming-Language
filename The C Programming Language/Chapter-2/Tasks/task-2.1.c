0b754ba5f3abbe215423ed3da6a4170183e5c2dd
SanyaUA <kozmey1996@mail.ua>
2018-06-28 15:21:46 +0300

    Prints out ranges of char, short, int and long

--- The C Programming Language/Chapter-2/Tasks/task-2.1.c
+++ The C Programming Language/Chapter-2/Tasks/task-2.1.c
@@ -5,33 +5,54 @@
 ** standard headers and by direct computation.
 **
 */
+#include <math.h>
 #include <stdio.h>
-#include <float.h>
 #include <limits.h>
 
 void ranges(void);
+void compute_ranges(void);
+
 int main()
 {
   ranges();
+  compute_ranges();
   return 0;
 }
-// Prints out ranges.
+// Prints out ranges from limits.h header.
 void ranges(void)
 {
-  printf("\t***** RANGES *****\n");
-  printf("CHAR_MIN = %d\n", CHAR_MIN);
-  printf("CHAR_MAX = %d\n", CHAR_MAX);
+  printf("\t***** RANGES FROM limits.h *****\n");
+  printf("CHAR_MIN  = %d\n", CHAR_MIN);
+  printf("CHAR_MAX  = %d\n", CHAR_MAX);
 
   printf("SCHAR_MIN = %d\n", SCHAR_MIN);
   printf("SCHAR_MAX = %d\n", SCHAR_MAX);
 
-  printf("SHRT_MIN = %d\n", SHRT_MIN);
-  printf("SHRT_MAX = %d\n", SHRT_MAX);
+  printf("SHRT_MIN  = %d\n", SHRT_MIN);
+  printf("SHRT_MAX  = %d\n", SHRT_MAX);
 
-  printf("INT_MIN = %d\n", INT_MIN);
-  printf("INT_MAX = %d\n", INT_MAX);
+  printf("INT_MIN   = %d\n", INT_MIN);
+  printf("INT_MAX   = %d\n", INT_MAX);
 
-  printf("LONG_MIN = %d\n", LONG_MIN);
-  printf("LONG_MAX = %d\n", LONG_MAX);
+  printf("LONG_MIN  = %ld\n", LONG_MIN);
+  printf("LONG_MAX  = %ld\n", LONG_MAX);
+}
+// Prints out ranges of char, short, int and long.
+void compute_ranges(void)
+{
+  int char_size = CHAR_BIT;
+  
+  printf("\t***** COMPUTED RANGES *****\n");
+  printf("CHAR_MIN = %d\n", -(int) pow(2, char_size - 1));
+  printf("CHAR_MAX = %d\n", (int) pow(2, char_size - 1) - 1);
+  
+  printf("SHRT_MIN = %d\n", -(int) pow(2,2 * char_size - 1));
+  printf("SHRT_MAX = %d\n", (int) pow(2, 2 * char_size - 1) - 1);
+
+  printf("INT_MIN  = %d\n", -(int) pow(2, 4 * char_size - 1));
+  printf("INT_MAX  = %d\n", (int) pow(2, 4 * char_size - 1) - 1);
+
+  printf("LONG_MIN = %ld\n", -(long) pow(8, 8 * char_size - 1));
+  printf("LONG_MAX = %ld\n", (long) pow(8, 8 * char_size - 1) - 1);
 }
 
